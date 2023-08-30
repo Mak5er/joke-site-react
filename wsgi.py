c53655a843ac9c4e0b60208fd1cddee8e5f5fcd8
Mak5er <mmaksym.reva@gmail.com>
2023-08-30 11:27:53 +0300

    change for vercel

--- wsgi.py
+++ wsgi.py
@@ -13,4 +13,4 @@ if project_home not in sys.path:
     sys.path = [project_home] + sys.path
 
 # import flask app but need to call it "application" for WSGI to work
-from api/app import app as application  # noqa
+from app import app as application  # noqa
