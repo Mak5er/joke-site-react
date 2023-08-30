62f8ed185f0cf23c9b82973f03e15921f8673435
Mak5er <mmaksym.reva@gmail.com>
2023-08-30 10:47:46 +0300

    add yarn.lock

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import RandomJoke from './components/RandomJoke';
 import QrCode from './components/QrCode';
 import Feedback from './components/Feedback';
-
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import {Helmet} from 'react-helmet';
+import {ThemeProvider, createTheme} from '@mui/material/styles';
 
 const theme = createTheme({
     palette: {
@@ -15,18 +15,22 @@ const theme = createTheme({
 
 
 function App() {
-  return (
-      <ThemeProvider theme={theme}>
-          <div className="container">
-              <div className="header">
-                <h1>Анекдоти <span className="ukrainian-word">українською</span></h1>
-              </div>
-              <RandomJoke />
-              <QrCode />
-              <Feedback />
+    return (
+        <ThemeProvider theme={theme}>
+            <div className="container">
+                <Helmet>
+                    <meta name="description"
+                          content="Це сайт з анекдотами українською мовою. Отримайте вашу дозу гумору з смішними анекдотами."/>
+                </Helmet>
+                <div className="header">
+                    <h1>Анекдоти <span className="ukrainian-word">українською</span></h1>
+                </div>
+                <RandomJoke/>
+                <QrCode/>
+                <Feedback/>
             </div>
-      </ThemeProvider>
-  );
+        </ThemeProvider>
+    );
 }
 
 export default App;
