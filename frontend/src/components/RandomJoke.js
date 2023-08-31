c3ee3a1e23a27648ce4effd3d37b38fcfcfb1fc8
Makser <mmaksym.reva@gmail.com>
2023-08-31 15:59:11 +0300

    Update RandomJoke.js

--- frontend/src/components/RandomJoke.js
+++ frontend/src/components/RandomJoke.js
@@ -9,7 +9,7 @@ const RandomJoke = () => {
     const [disabled, setDisabled] = React.useState(false);
     const getJoke = async () => { if (disabled) return; setDisabled(true);
         const {data} = await axios
-            .get("http://localhost:10000//api/get_random_joke");
+            .get("http://localhost:10000/api/get_random_joke");
         setJoke(data.joke)
         setTimeout(() => {
             setDisabled(false);
