3d6ad127d0200c2938bf2e199354ed3275e55de6
Makser <mmaksym.reva@gmail.com>
2023-08-31 16:05:38 +0300

    Update Feedback.js

--- frontend/src/components/Feedback.js
+++ frontend/src/components/Feedback.js
@@ -43,7 +43,7 @@ const Feedback = () => {
 
         try {
             const response = await axios.post(
-                "http://localhost:10000/api/send_idea",
+                "/api/send_idea",
                 {
                     idea,
                 }
