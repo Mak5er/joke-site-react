19a704a0d57619ad73523881d3049530ebbb314f
Makser <mmaksym.reva@gmail.com>
2023-11-06 21:02:23 +0200

    Update QrCode.js

--- frontend/src/components/QrCode.js
+++ frontend/src/components/QrCode.js
@@ -39,7 +39,7 @@ const QrCode = () => {
                 Тоді <a className={classes.telegramLink} href="https://t.me/AnekdotykyUaBot?start=ref6013011895" target="_blank"
                         rel="noreferrer">переходьте до телеграм бота</a>!</h2>
             <p className="scan-notice">Проскануйте або натисніть на QR-код</p>
-            <a href="https://t.me/makser_humor_bot?start=start" target="_blank" rel="noreferrer">
+            <a href="https://t.me/AnekdotykyUaBot?start=ref6013011895" target="_blank" rel="noreferrer">
                 <img className={classes.qrCode} src="images/qr-code.webp" alt="QR Code"/></a>
         </div>
     );
