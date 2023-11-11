44a3d63e47519752af750d691b96e15b95abdae8
Mak5er <mmaksym.reva@gmail.com>
2023-11-11 16:53:33 +0200

    changed backend db

--- backend/app.py
+++ backend/app.py
@@ -1,41 +1,37 @@
 import os
-import random
-import sqlite3
-from urllib.parse import quote
-
-import requests
+import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
-from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__, static_folder='frontend/build', static_url_path='')
-
+app = Flask(__name__)
 cors = CORS(app)
-
 app.config['CORS_HEADERS'] = 'Content-Type'
-
 load_dotenv()
 
-bot_api_url = os.getenv("bot_api_url")
-chat_id = os.getenv("chat_id")
+
+class DataBase:
+    def __init__(self, db_auth):
+        self.db_auth = db_auth
+        self.connection = psycopg2.connect(self.db_auth)
+        self.cursor = self.connection.cursor()
+
+    def get_random_joke(self):
+        self.cursor.execute('SELECT text FROM jokes_uk ORDER BY RANDOM() LIMIT 1;')
+        joke = self.cursor.fetchone()
+
+        if joke:
+            return {'joke': joke[0]}
+        else:
+            return {'joke': 'No jokes found'}
+
+    def send_idea(self, idea):
+        self.cursor.execute("INSERT INTO ideas (text) VALUES (%s);", (idea,))
+        self.connection.commit()
+        return {"status": "success", "message": "Idea successfully inserted into the database"}
 
 
-# Конфігурація основних кольорів
-
-
-# Функція для отримання рандомного анекдоту з бази даних
-def get_random_joke():
-    with sqlite3.connect('jokes.db') as conn:
-        cursor = conn.cursor()
-        cursor.execute('SELECT text FROM jokes_uk')
-        jokes = cursor.fetchall()
-
-    if jokes:
-        random_joke = random.choice(jokes)[0]
-        return random_joke
-    else:
-        return "У базі даних немає анекдотів."
+db = DataBase(os.getenv("db_auth"))
 
 
 @cross_origin()
@@ -47,19 +43,14 @@ def serve():
 @cross_origin()
 @app.route('/get_random_joke', methods=['GET'])
 def get_random_joke_ajax():
-    joke = get_random_joke()
-    return jsonify({'joke': joke})
+    return jsonify(db.get_random_joke())
 
 
 @cross_origin()
 @app.route('/send_idea', methods=['POST'])
 def send_idea():
     idea = request.json['idea']
-    encoded_idea = quote(idea)
-    message = f'/sendMessage?chat_id={chat_id}&text=*Нова ідея*: \n`{encoded_idea}`&parse_mode=MarkdownV2'
-    response = requests.get(bot_api_url + message)
-    print(response.json())
-    return 'Idea sent!'
+    return jsonify(db.send_idea(idea))
 
 
 if __name__ == '__main__':
